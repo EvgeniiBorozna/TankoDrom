ca9fc4a16ae4fe1d5e481454caf790caab5091b7
EvgeniiBorozna <borozna@ya.ru>
2023-07-18 12:27:52 +0300

    18.07

---
+++ Source/Tankodrom/TankFactory.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "TankPawn.h"
+#include "Engine/TargetPoint.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TankFactory.generated.h"
+
+UCLASS()
+class TANKODROM_API ATankFactory : public AActor, public IDamageTaker
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UStaticMeshComponent* BuildingMesh;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UArrowComponent* TankSpawnPoint;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UBoxComponent* HitCollider;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+		UHealthComponent* HealthComponent;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn tanks params")
+		TSubclassOf<ATankPawn> SpawnTankClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Spawn tanks params")
+		float SpawnTankRate = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn tanks params")
+		TArray<ATargetPoint*> TankWayPoints;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn tanks params")
+		//AMapLoader* LinkedMapLoader;
+
+public:
+	// Sets default values for this actor's properties
+	ATankFactory();
+	UFUNCTION()
+		void TakeDamage(FDamageData DamageData);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void SpawnNewTank();
+
+
+	UFUNCTION()
+		void Die();
+
+	UFUNCTION()
+		void DamageTaked(float DamageValue);
+};
